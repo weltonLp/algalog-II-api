f5738fa6f3e131292bf7c457fef858624a6bc043
Welton Leite <weltonrcc@hotmail.com>
2023-05-15 07:59:45 -0300

    Criacao do projeto

---
+++ src/main/java/com/algaworks/algalog/AlgalogIiApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgalogIiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgalogIiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algalog/AlgalogIiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgalogIiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
