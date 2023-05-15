ea7eb1b94642d15f47f54435ed44fbaebf3473b1
Welton Leite <weltonrcc@hotmail.com>
2023-05-15 15:40:05 -0300

    Usando Spring Data JPA

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -5,24 +5,34 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+
+	//AUTOWIRED DIFICULTA MOCK - por isso podemos usar construtor ou 
+	//	@AllArgsConstructor no início da classe
+//	public ClienteController(ClienteRepository repository) {
+//		super();
+//		this.repository = repository;
+//	}
 	
+	@Autowired
+	private ClienteRepository repository;
+	
+			
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
-		
+		return repository.findAll();	
 	}
+	
+		
 	
 	
 }
