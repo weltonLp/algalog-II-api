71d30f823b0b1291001d88a36eb591e6a2ae5e94
Welton Leite <weltonrcc@hotmail.com>
2023-05-17 07:42:27 -0300

    Validações em Cascatas e Validation Groups

--- src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -11,9 +11,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 
+import com.algaworks.algalog.domain.ValidationGroups;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -25,17 +31,24 @@ import lombok.Setter;
 @Entity
 public class Entrega {
 	
+	
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Valid
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class)
+	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 	
+	@Valid
+	@NotNull
 	@Embedded
 	private Destinatario destinatario;
 	
+	@NotNull
 	private BigDecimal taxa;
 	
 	@JsonProperty(access = Access.READ_ONLY)
